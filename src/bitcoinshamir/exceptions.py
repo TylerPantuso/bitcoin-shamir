babf2783d0d58aa8b1fd4538ac304ce71ad18bfe
tylerpantuso <tp10krunner@yahoo.com>
2022-12-01 07:53:06 -0600

    Added exception dunders

--- src/bitcoinshamir/exceptions.py
+++ src/bitcoinshamir/exceptions.py
@@ -1,9 +1,33 @@
 class ChecksumError(Exception):
-    pass
+    """
+    Exception raised when a checksum is invalid.
+    """
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+    def __str__(self) -> str:
+        return super().__str__()
 
 class ThresholdError(Exception):
-    pass
+    """
+    Exception raised when number of shares provided do not meet the threshold.
+    """
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+    def __str__(self) -> str:
+        return super().__str__()
 
 class LanguageError(Exception):
-    pass
+    """
+    Exception raised when a word or language is not found.
+    """
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+    def __str__(self) -> str:
+        return super().__str__()
 
